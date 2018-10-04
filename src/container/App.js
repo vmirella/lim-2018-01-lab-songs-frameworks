0834578df1f77c72a401d74534d99d6548eaa35c
vmirella <vmirella_17@hotmail.com>
2018-10-04 18:34:59 -0500

    Comenzando a organizar mis .componentes

--- src/container/App.js
+++ src/container/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Button } from 'reactstrap';
+import { Form, Row, Col, Button } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Form>
+        <Row form>
+          <Col md={3}>
+          </Col>
+        </Row>
+      </Form>
     );
   }
 }
