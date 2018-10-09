c37fc65b3e8033bbeedb999bcd877a1ca4c716c3
vmirella <vmirella_17@hotmail.com>
2018-10-09 05:14:29 -0500

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './container/App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
